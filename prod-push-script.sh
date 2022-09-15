26875aaef8c62f54ae714b5093d795459ba3254e
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2022-09-15 16:27:08 +0300

    Fix script

--- prod-push-script.sh
+++ prod-push-script.sh
@@ -3,7 +3,15 @@
 git config --global user.email "you@example.com"
 git config --global user.name "GitHub Action"
 
-ls -la
 cd ..
 ls -la
+
+git clone https://github.com/Kornyshev/ReactSiteProdBuild.git
+cd ReactSiteProdBuild/
+ls -la
+
+cp -r -f ../ReactSite/build/* .
 git status
+git add .
+git commit -am "Release commit"
+git push
