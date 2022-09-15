587dad08f127bff75a1291fbf5bcc895164101ee
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2022-09-15 14:54:24 +0300

    Fixes in script - 2

--- prod-push-script.sh
+++ prod-push-script.sh
@@ -9,14 +9,20 @@ cd prod/
 echo "Git init"
 git init
 
+echo "Config Git"
+git config --global user.email "you@example.com"
+git config --global user.name "GitHub Action"
+
 echo "Add remoter repo and pull"
 git remote add origin https://github.com/Kornyshev/ReactSiteProdBuild.git
-git pull
+git pull origin master
+ls -la
 
 echo "Copy build files to prod/"
 cp -r ../build/* .
 
 echo "Commit and push"
+git branch --set-upstream-to=origin/master master
 git add .
 git commit -m "Release commit"
-git push
+git push --set-upstream origin master
