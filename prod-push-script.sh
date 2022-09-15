dfda2517850e31506bd1aaab47c51abcf5e827c9
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2022-09-15 16:12:09 +0300

    Fix script

--- prod-push-script.sh
+++ prod-push-script.sh
@@ -1,31 +1,30 @@
 #! /bin/bash
 
-echo "\nCreating dir and initializing Git repo\n"
+echo "Creating dir and initializing Git repo"
 mkdir prod
 cd prod/
 git init
 git config --global user.email "you@example.com"
 git config --global user.name "GitHub Action"
-git remote add origin https://github.com/Kornyshev/ReactSiteProdBuild.git
+git remote add origin git@github.com:Kornyshev/ReactSiteProdBuild.git
 echo "=========================="
 
 git pull origin master
-echo "\nPulled repository:\n"
+echo "Pulled repository:"
 ls -la
 echo "=========================="
 
-echo "\nCopy build files to prod/\n"
+echo "Copy build files to prod/"
 cp -r -f ../build/* .
-ls -la
 echo "=========================="
 
-echo "\nCommit\n"
+echo "Commit"
 git branch --set-upstream-to=origin/master master
 git add .
 git commit -m "Release commit"
 ls -la
 echo "=========================="
 
-echo "\nPush\n"
+echo "Push"
 git push -u origin
 echo "=========================="
