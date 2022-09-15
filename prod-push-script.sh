f5ad9a6037fd461236df7c8ecaebadc8de85aa77
Evgeniy Kornyshev <Evgeniy_Kornyshev@epam.com>
2022-09-15 16:23:21 +0300

    Fixes + delete prod files

--- prod-push-script.sh
+++ prod-push-script.sh
@@ -1,27 +1,9 @@
 #! /bin/bash
 
-echo "Creating dir and initializing Git repo"
-mkdir prod
-cd prod/
-git clone https://github.com/Kornyshev/ReactSiteProdBuild.git
 git config --global user.email "you@example.com"
 git config --global user.name "GitHub Action"
-cd ReactSiteProdBuild/
-pwd
-echo "Cloned repository:"
+
 ls -la
-echo "=========================="
-
-echo "Copy build files to prod/"
-cp -r -f ../../build/* .
-echo "=========================="
-
-echo "Commit"
-git add .
-git commit -am "Release commit"
+cd ..
 ls -la
-echo "=========================="
-
-echo "Push"
-git push -u origin master
-echo "=========================="
+git status
